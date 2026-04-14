7b2f4254fbbfddc66f000236a0234cbe387d2ba9
eric <eric55339944@gmail.com>
2026-04-14 13:12:13 +0800

    簡化index寫法

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MyFitnessCoach from '@/views/MyFitnessCoach.vue'
 import Store from '@/views/Store.vue'
 
-const routes = [
-  { path: '/', component: MyFitnessCoach },
-  { path: '/store', component: Store },
-]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+  { path: '/', component: MyFitnessCoach },
+  { path: '/store', component: Store },
+]
 })
 
 export default router
